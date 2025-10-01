bafe3a9f0c1f885956febeaf8c2565b76ae54fe2
dhanikajoshi-create <dhanika.joshi@krvia.ac.in>
2025-10-01 09:49:11 +0530

    Add files via upload

---
+++ data/3Waterwayswaterways_5.js
@@ -0,0 +1,8 @@
+var json_3Waterwayswaterways_5 = {
+"type": "FeatureCollection",
+"name": "3Waterwayswaterways_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "id": "1" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 72.849357339378557, 19.112832783894586 ], [ 72.849515461176381, 19.112842585408831 ], [ 72.849724224264236, 19.112912679200228 ], [ 72.849768865427663, 19.112979051170001 ], [ 72.849837140148225, 19.113122340098005 ], [ 72.849942099999978, 19.1131764 ] ] ] } }
+]
+}
---
+++ data/5Openopenspaces_3.js
@@ -0,0 +1,10 @@
+var json_5Openopenspaces_3 = {
+"type": "FeatureCollection",
+"name": "5Openopenspaces_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "id": "52", "type": "open", "area": 1288.13, "plot_no": null }, "geometry": { "type": "MultiPolygon", "coordinates": [ ] } },
+{ "type": "Feature", "properties": { "fid": "2", "id": "53", "type": "open", "area": 1392.28, "plot_no": null }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ 72.85049472594946, 19.113092696217883 ], [ 72.850749015815595, 19.113051560131318 ], [ 72.850701521988441, 19.112858033404915 ], [ 72.85062236560988, 19.112849619194286 ], [ 72.850305740095649, 19.112961808634196 ], [ 72.850229552081274, 19.113030992084198 ], [ 72.850164650465544, 19.113324752473144 ], [ 72.85049472594946, 19.113092696217883 ] ] ] ] } },
+{ "type": "Feature", "properties": { "fid": "3", "id": "54", "type": "open", "area": 5033.95, "plot_no": null }, "geometry": { "type": "MultiPolygon", "coordinates": [ ] } }
+]
+}
